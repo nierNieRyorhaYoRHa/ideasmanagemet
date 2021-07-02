44ba876e958db2a56819a1b953e7f5d2426944d7
nierNieRyorhaYoRHa <t.ishida.tech@gmail.com>
2021-07-02 14:40:01 +0900

    マイグレーション

--- db/migrate/20210702053407_create_ideas.rb
+++ db/migrate/20210702053407_create_ideas.rb
@@ -1,6 +1,8 @@
 class CreateIdeas < ActiveRecord::Migration[6.0]
   def change
     create_table :ideas do |t|
+      t.references :category, null: false,  foreign_key: true
+      t.text       :body,     null: false
 
       t.timestamps
     end
